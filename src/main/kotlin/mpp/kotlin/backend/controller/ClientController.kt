b4c944911577868875529bb87c1e7c09471b3deb
TurcaVasileIonut <turcavasile235711@gmail.com>
2023-05-21 21:10:41 +0300

    Added cross origin on all controllers

--- src/main/kotlin/mpp/kotlin/backend/controller/ClientController.kt
+++ src/main/kotlin/mpp/kotlin/backend/controller/ClientController.kt
@@ -3,12 +3,10 @@ package mpp.kotlin.backend.controller
 import domain.Client
 import mpp.kotlin.backend.service.ClientService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
+@CrossOrigin(origins = ["*"])
 @RequestMapping("/clients")
 class ClientController {
 
--- src/main/kotlin/mpp/kotlin/backend/controller/EmployeeController.kt
+++ src/main/kotlin/mpp/kotlin/backend/controller/EmployeeController.kt
@@ -3,11 +3,13 @@ package mpp.kotlin.backend.controller
 import domain.Employee
 import mpp.kotlin.backend.service.EmployeeService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@CrossOrigin(origins = ["*"])
 @RequestMapping("/employees")
 class EmployeeController {
 

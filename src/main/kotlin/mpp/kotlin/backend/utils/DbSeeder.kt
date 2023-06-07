d1750c4212bb2fcb3c10e1fa3a882090db199ff0
Vincze Iulia-Paula <iulia.vincze@stud.ubbcluj.ro>
2023-06-07 10:48:10 +0300

    More realistic db

--- src/main/kotlin/mpp/kotlin/backend/utils/DbSeeder.kt
+++ src/main/kotlin/mpp/kotlin/backend/utils/DbSeeder.kt
@@ -8,6 +8,7 @@ import java.security.MessageDigest
 import java.time.LocalDate
 import java.util.*
 import javax.xml.bind.DatatypeConverter
+import kotlin.random.Random
 
 @Component
 class DbSeeder(
@@ -27,6 +28,14 @@ class DbSeeder(
 
     override fun run(vararg args: String?) {
 
+        val random = Random
+        val counties = listOf("County A", "County B", "County C", "County D", "County E")
+        val cities = listOf("City X", "City Y", "City Z")
+        val streets = listOf("Street P", "Street Q", "Street R", "Street S", "Street T")
+        val zipCodes = listOf("12345", "54321", "67890")
+        val lastNames = listOf("Smith", "Johnson", "Brown", "Davis", "Miller")
+        val firstNames = listOf("John", "Emily", "Michael", "Olivia", "William")
+
         this.employeeRepository.deleteAll()
         this.clientRepository.deleteAll()
         this.adminRepository.deleteAll()
@@ -35,30 +44,40 @@ class DbSeeder(
         this.invoiceRepository.deleteAll()
 
         for (i in 1..10) {
-            val address = Address("Test$i", "Test$i", "Test$i", i, "1234")
-            this.addressRepository.save(address)
+            val county = counties[random.nextInt(counties.size)]
+            val city = cities[random.nextInt(cities.size)]
+            val street = streets[random.nextInt(streets.size)]
+            val zipCode = zipCodes[random.nextInt(zipCodes.size)]
+            val lastName = lastNames[random.nextInt(lastNames.size)]
+            val firstName = firstNames[random.nextInt(firstNames.size)]
 
-            val admin = Admin("Test$i", "Test$i", "07noidoi", "test$i@mpp.com", encryptPassword("admin$i"), address)
-            this.adminRepository.save(admin)
+            val address = Address(county, city, street, i, zipCode)
+            addressRepository.save(address)
 
-            val client = Client(
-                "Test$i", "Test$i", "07noidoi", "test$i@mpp.com", encryptPassword("client$i"), i.toFloat(), address
-            )
-            this.clientRepository.save(client)
+            val admin = Admin("$lastName$i", "$firstName$i", "+40758509793", "test$i@mpp.com", encryptPassword("admin$i"), address)
+            adminRepository.save(admin)
 
-            val employee =
-                Employee("Test$i", "Test$i", "07noidoi", "test$i@mpp.com", encryptPassword("employee$i"), address)
-            this.employeeRepository.save(employee)
+            val client = Client("$lastName$i", "$firstName$i", "+40758509793", "test$i@mpp.com", encryptPassword("client$i"), i.toFloat(), address)
+            clientRepository.save(client)
 
-            val item = Item("Item$i", i.toFloat(), CategoryType.PAPER_AND_CARDBOARD)
-            this.itemRepository.save(item)
+            val employee = Employee("$lastName$i", "$firstName$i", "+40758509793", "test$i@mpp.com", encryptPassword("employee$i"), address)
+            employeeRepository.save(employee)
 
-            val invoice = Invoice(
-                CategoryType.PAPER_AND_CARDBOARD, (i * 10).toFloat(), i.toFloat(), LocalDate.now(), client, employee
-            )
-            val savedInvoice = this.invoiceRepository.save(invoice)
+            val itemName = "Item $i"
+            val itemPrice = i.toFloat()
+            val categoryType = CategoryType.values().random()
+            val item = Item(itemName, itemPrice, categoryType)
+            itemRepository.save(item)
+
+            val invoiceCategoryType = CategoryType.values().random()
+            val totalAmount = (i * 10).toFloat()
+            val quantity = i.toFloat()
+            val invoiceDate = LocalDate.now()
+
+            val invoice = Invoice(invoiceCategoryType, totalAmount, quantity, invoiceDate, client, employee)
+            val savedInvoice = invoiceRepository.save(invoice)
             savedInvoice.items.add(Content(ContentId(savedInvoice.getId(), item.getId()), savedInvoice, item, i))
-            this.invoiceRepository.save(savedInvoice)
+            invoiceRepository.save(savedInvoice)
         }
 
         println(" -- Database has been initialized")
